c995d724b7d76e26ba38c167d299fb9721e7f06d
pixelstopresence <pixelstopresence@gmail.com>
2026-01-30 10:35:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Query
+import json
+import random
+
+app = FastAPI(title="Interview AI API")
+
+with open("data.json", "r") as f:
+    DATA = json.load(f)
+
+
+@app.get("/")
+def home():
+    return {"status": "Interview AI API running 🚀"}
+
+
+@app.get("/generate")
+def generate_questions(
+    category: str = Query(...),
+    level: str = Query(...),
+    count: int = Query(1)
+):
+    category = category.lower()
+    level = level.lower()
+
+    if category not in DATA:
+        return {"error": "Invalid category"}
+
+    filtered = [
+        q for q in DATA[category]
+        if q["level"] == level
+    ]
+
+    if not filtered:
+        return {"error": "No questions found"}
+
+    random.shuffle(filtered)
+    return {
+        "category": category,
+        "level": level,
+        "questions": filtered[:count]
+    }
